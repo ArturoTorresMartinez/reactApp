a61fb0c389acb43f8ccb711986db68be59f55879
Arturo Torres <efrainarturotorres@gmail.com>
2019-05-26 22:07:05 -0500

    fixed ImageUrl

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -183,6 +183,28 @@ class Feed extends Component {
         `
         };
 
+        if (this.state.editPost) {
+          graphqlQuery = {
+            query: ` 
+mutation {
+            updatePost(id: "${this.state.editPost._id}", postInput: {title: "${postData.title}", content: "${
+              postData.content
+              }", imageUrl: "${imageUrl}"}) {
+              _id
+              title
+              content
+              imageUrl
+              creator {
+                name
+              }
+              createdAt
+            }
+          }
+            
+            `
+          }
+        }
+
         return fetch('http://localhost:8080/graphql', {
           method: 'POST',
           body: JSON.stringify(graphqlQuery),
@@ -202,16 +224,19 @@ class Feed extends Component {
           );
         }
         if (resData.errors) {
-          throw new Error('User login failed!');
+          throw new Error('Post loading failed!');
         }
-        console.log(resData);
+        let responseData = 'createPost';
+        if (this.state.editPost) {
+          responseData = 'updatePost';
+        }
         const post = {
-          _id: resData.data.createPost._id,
-          title: resData.data.createPost.title,
-          content: resData.data.createPost.content,
-          creator: resData.data.createPost.creator,
-          createdAt: resData.data.createPost.createdAt,
-          imagePath: resData.data.createPost.imageUrl
+          _id: resData.data[responseData]._id,
+          title: resData.data[responseData].title,
+          content: resData.data[responseData].content,
+          creator: resData.data[responseData].creator,
+          createdAt: resData.data[responseData].createdAt,
+          imagePath: resData.data[responseData].imageUrl
         };
         this.setState(prevState => {
           let updatedPosts = [...prevState.posts];
@@ -221,7 +246,9 @@ class Feed extends Component {
             );
             updatedPosts[postIndex] = post;
           } else {
-            updatedPosts.pop();
+            if (prevState.posts.length >= 2) {
+              updatedPosts.pop();
+            }
             updatedPosts.unshift(post);
           }
           return {
@@ -249,19 +276,28 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch('http://localhost:8080/feed/post/' + postId, {
-      method: 'DELETE',
-      headers: {
-        Authorization: 'Bearer ' + this.props.token
+    const graphqlQuery = {
+      query: `
+      mutation{
+        deletePost(id: "${postId}")
       }
+      `
+    }
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(graphqlQuery)
     })
       .then(res => {
-        if (res.status !== 200 && res.status !== 201) {
-          throw new Error('Deleting a post failed!');
-        }
         return res.json();
       })
       .then(resData => {
+        if (resData.errors) {
+          throw new Error('Post deletion failed!');
+        }
         console.log(resData);
         this.loadPosts();
         // this.setState(prevState => {
